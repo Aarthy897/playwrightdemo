a1bb7af9213760d0bda16dd18237097ab1982f3b
Aarthy897 <aarthy897@gmail.com>
2026-04-27 12:55:05 +0530

    second message

--- Reversestring.js
+++ Reversestring.js
@@ -7,3 +7,9 @@ function reverseString(str) {
   return reversed;
 }
 console.log(reverseString("JavaScript")); // "tpircSavaJ"
+//
+console.log("Test 1")
+console.log("Test 2")
+console.log("Test 3")
+console.log("Test 4")
+console.log("Test 5")
